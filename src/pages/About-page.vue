673a90957d1e0783ebdb44d2f90e445071b9a734
l050101 <l01062506156@gmail.com>
2022-03-07 23:34:47 +0900

    first

---
+++ src/pages/About-page.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>About page</h1>
+    </div>
+</template>
+<script>
+import '@/assets/style.css'
+export default {
+    
+}
+</script>
+    
+<style lang="">
+</style>
---
+++ src/pages/Home-page.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Home page</h1>
+    </div>
+</template>
+<script>
+import '@/assets/style.css'
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
